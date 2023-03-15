8f982dd5fa24380851f97778ca67ba6ac842f7e9
Joost Loohuis <joost@loohuis.dev>
2023-03-15 13:28:25 +0100

    Initial Setup

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    'semi': [2, "always"],
+    "comma-dangle": ["error", "always-multiline"],
+  },
+};
