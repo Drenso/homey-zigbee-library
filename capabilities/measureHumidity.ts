ba97e15e8a72c4772c071b8a5a9f1b02c91e0063
Joost Loohuis <joost@loohuis.dev>
2023-03-27 14:34:53 +0200

    Add measure humidity capability

---
+++ capabilities/measureHumidity.ts
@@ -0,0 +1,22 @@
+import {ZigBeeDevice} from 'homey-zigbeedriver';
+import {CLUSTER, ZCLNode} from 'zigbee-clusters';
+import {initReadOnlyCapability} from '../lib/attributeDevice';
+
+export default async function initMeasureHumidityDevice(
+  device: ZigBeeDevice,
+  zclNode: ZCLNode,
+  capabilityId = 'measure_humidity',
+  endpointId?: number,
+): Promise<void> {
+  await initReadOnlyCapability(
+    device,
+    zclNode,
+    capabilityId,
+    CLUSTER.RELATIVE_HUMIDITY_MEASUREMENT,
+    'measuredValue',
+    (value: number) => Math.round((value / 100) * 10) / 10,
+    30,
+    undefined,
+    endpointId,
+  );
+}
