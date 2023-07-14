cb84bfce9d988c4989b84d40f29e8dac7a09d3b1
Joost Loohuis <joost@loohuis.dev>
2023-07-14 13:36:54 +0200

    Add target_temperature capability

---
+++ capabilities/targetTemperature.ts
@@ -0,0 +1,46 @@
+import {
+  initReadWriteCapability,
+  ReadOnlyArgumentOverrides,
+} from '../lib/attributeDevice';
+import {ZigBeeDevice} from 'homey-zigbeedriver';
+import {CLUSTER, ZCLNode} from 'zigbee-clusters';
+
+type ArgumentOverrides = ReadOnlyArgumentOverrides;
+
+export default async function initTargetTemperatureDevice(
+  device: ZigBeeDevice,
+  zclNode: ZCLNode,
+  {
+    capabilityId = 'target_temperature',
+    cluster = CLUSTER.THERMOSTAT,
+    attributeName = 'occupiedHeatingSetpoint',
+    minChange = 10,
+    maxInterval,
+    endpointId,
+  }: Partial<ArgumentOverrides> = {},
+): Promise<void> {
+  await initReadWriteCapability(
+    device,
+    zclNode,
+    capabilityId,
+    cluster,
+    attributeName,
+    (value: number) => {
+      // Value comes from int16
+      // Check for invalid values
+      if (value == 0x8000) return null;
+      if (value < -0x154D || value > 0x7FFE) {
+        device.error('Temperature value outside valid range');
+        return null;
+      }
+
+      // MeasuredValue represents the temperature in degrees Celsius as follows:
+      // MeasuredValue = 100 x Temperature in degrees Celsius
+      return Math.round((value / 100) * 10) / 10;
+    },
+    (value) => Math.round(value * 100),
+    minChange,
+    maxInterval,
+    endpointId,
+  );
+}
