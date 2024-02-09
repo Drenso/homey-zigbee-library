f84ba43be5498eb6b0d31451f25a8713d37c7b71
Joost Loohuis <joost@loohuis.dev>
2024-02-09 13:42:37 +0100

    Add locked capability

---
+++ capabilities/locked.ts
@@ -0,0 +1,38 @@
+import { ZigBeeDevice } from 'homey-zigbeedriver';
+import { CLUSTER, ZCLNode } from 'zigbee-clusters';
+import {LockState} from "../lib/clusters/DoorLockCluster";
+import {initReadCommandCapability} from "../lib/attributeDevice";
+
+type ArgumentOverrides = {
+  capabilityId: string,
+  reportParser: (value: LockState) => unknown,
+  endpointId?: number,
+};
+
+export default async function initLockedDevice(
+  device: ZigBeeDevice,
+  zclNode: ZCLNode,
+  {
+    capabilityId = 'locked',
+    // Can be extended to handle the 'not_fully_locked' state
+    reportParser = (value: LockState): boolean => value == 'locked',
+    endpointId,
+  }: Partial<ArgumentOverrides> = {},
+): Promise<void> {
+  const command = (value: boolean): string => value ? 'lockDoor' : 'unlockDoor';
+  // Return empty object, the command specifies the action for this cluster ('lockDoor'/unlockDoor')
+  const commandArgParser = (): Record<string, never> => ({});
+
+  await initReadCommandCapability(
+    device,
+    zclNode,
+    capabilityId,
+    CLUSTER.DOOR_LOCK,
+    command,
+    commandArgParser,
+    'lockState',
+    reportParser,
+    undefined,
+    endpointId,
+  );
+}
