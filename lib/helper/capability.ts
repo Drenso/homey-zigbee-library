0c2c26a39e80bf0b788cfddce0e31a6db96381a4
Tobias Feijten <tobias@drenso.nl>
2024-04-17 08:37:24 +0000

    Add capability helper

---
+++ lib/helper/capability.ts
@@ -0,0 +1,17 @@
+import {Device} from 'homey';
+
+export async function addCapabilityIfNotExists(device: Device, capabilityId: string): Promise<void> {
+  if (device.hasCapability(capabilityId)) {
+    return;  
+  }
+
+  await device.addCapability(capabilityId);
+}
+
+export async function removeCapabilityIfExists(device: Device, capabilityId: string): Promise<void> {
+  if (!device.hasCapability(capabilityId)) {
+    return;
+  }
+
+  await device.removeCapability(capabilityId);
+}
